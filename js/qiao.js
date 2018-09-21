6a86eb183c7b615c1af7fea40c99ef4767622d42
X1805476752 <42667563+X1805476752@users.noreply.github.com>
2018-09-21 17:46:16 +0800

    乔镕基测试
    说的是

---
+++ js/qiao.js
@@ -0,0 +1,13 @@
+// $(".hovers ul li").on("mouseover",function(){
+//     // alert("hah");
+//     $(this).css({
+//         "transform": "scale(1.3)",
+//         "background-color": "rgb(167,207,83)"
+//     })
+// })
+// $(".hovers ul li").on("mouseout", function () {
+//     // alert("hah");
+//     $(this).css({
+//         "transform": "scale(1)",
+//     })
+// })
